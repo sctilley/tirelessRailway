7088b92ef1c77aace90ba96bb3bdd4b8adc18d11
sctilley <sctilley@gmail.com>
2022-07-20 22:07:16 +0800

    allowed hosts

--- tireless/settings.py
+++ tireless/settings.py
@@ -9,13 +9,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l#50pz!rr_g9eu5hp7i2u6^fzaxgn1kn(uniapgb6=_etfgslb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+ALLOWED_HOSTS = ["tirelesstracker.herokuapp.com"]
 
 
 # Application definition
