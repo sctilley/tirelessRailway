bd1421dfbdd8aa5ddf31b298a242e0477420df95
sctilley <sctilley@gmail.com>
2022-07-26 15:59:16 +0800

    ssl redirect

--- tireless/settings.py
+++ tireless/settings.py
@@ -176,3 +176,5 @@ django_heroku.settings(locals())
 
 if os.getcwd() == '/app':
     DEBUG = False
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
