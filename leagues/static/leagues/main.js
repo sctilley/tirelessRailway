2efb4c369a4e1f03c220add040fbddfc7cca8f52
sctilley <sctilley@gmail.com>
2022-11-17 07:14:16 +0800

    local dev database

--- leagues/static/leagues/main.js
+++ leagues/static/leagues/main.js
@@ -1,5 +1,9 @@
 const hamburger = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mobile-menu');
 
 hamburger.addEventListener('click', function () {
-    this.classList.toggle('is-active')
+    this.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-open');
 })
+
+console.log("javascrip working now");
