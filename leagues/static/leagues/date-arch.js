d5557302ecead7539df778f92f41cbc0ab1fc180
sctilley <sctilley@gmail.com>
2022-12-13 19:12:42 +0800

    league edit popup better

--- leagues/static/leagues/date-arch.js
+++ leagues/static/leagues/date-arch.js
@@ -1,29 +1,38 @@
-document.getElementById('datebutton1').onclick = openDate;
-document.getElementById('datebutton2').onclick = openDate;
-document.getElementById('datebutton3').onclick = openDate;
-document.getElementById('datebutton4').onclick = openDate;
-document.getElementById('datebutton5').onclick = openDate;
-
-
-function openDate(){
-  console.log("button clicked, id "+this.id);
-  this.classList.toggle('hidden');
-  this.nextElementSibling.classList.toggle('hidden');
-  this.nextElementSibling.nextElementSibling.classList.toggle('hidden');
-}
-
-document.getElementById('archbutton1').onclick = openArch;
-document.getElementById('archbutton2').onclick = openArch;
-document.getElementById('archbutton3').onclick = openArch;
-document.getElementById('archbutton4').onclick = openArch;
-document.getElementById('archbutton5').onclick = openArch;
-
-function openArch(){
-  console.log("Arch button clicked, id "+this.id);
-  this.classList.toggle('hidden');
-  this.nextElementSibling.classList.toggle('hidden');
+function clickshow(butt) {
+  console.log("button clicked, id "+butt);
+  document.getElementById(butt).classList.toggle('hidden');
+  document.getElementById(butt).nextElementSibling.classList.toggle('hidden');
+  document.getElementById(butt).nextElementSibling.nextElementSibling.classList.toggle('hidden');
 }
 
 
-console.log("javascrip working now");
+
+// document.getElementById('datebutton1').onclick = openDate;
+// document.getElementById('datebutton2').onclick = openDate;
+// document.getElementById('datebutton3').onclick = openDate;
+// document.getElementById('datebutton4').onclick = openDate;
+// document.getElementById('datebutton5').onclick = openDate;
+
+
+// function openDate(){
+//   console.log("button clicked, id "+this.id);
+//   this.classList.toggle('hidden');
+//   this.nextElementSibling.classList.toggle('hidden');
+//   this.nextElementSibling.nextElementSibling.classList.toggle('hidden');
+// }
+
+// document.getElementById('archbutton1').onclick = openArch;
+// document.getElementById('archbutton2').onclick = openArch;
+// document.getElementById('archbutton3').onclick = openArch;
+// document.getElementById('archbutton4').onclick = openArch;
+// document.getElementById('archbutton5').onclick = openArch;
+
+// function openArch(){
+//   console.log("Arch button clicked, id "+this.id);
+//   this.classList.toggle('hidden');
+//   this.nextElementSibling.classList.toggle('hidden');
+// }
+
+
+// console.log("javascrip working now");
 
